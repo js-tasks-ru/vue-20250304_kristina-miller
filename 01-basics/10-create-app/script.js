0ae144d6ad18f44ebee5b9d198130d6b78019e8e
Krisztina Lengyel <krisztin.lengyel@gmail.com>
2025-03-16 15:52:20 +0100

    Solved two homeworks, Create app and Weather app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,14 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp, computed } from 'vue'
+
+const App = defineComponent({
+    name: 'App',
+    setup() {
+        const formattedDate = computed(() => new Date().toLocaleDateString(navigator.language, { dateStyle: "long" }));
+
+        return { formattedDate };
+    },
+
+    template: '<p> Сегодня {{formattedDate }}</p>'
+})
+
+const app = createApp(App).mount('#app');
